edd70619f3a6192c1f2da376434b12a52422e380
E19B287H <E19B287H>
2020-02-26 13:50:17 +0100

    adding first models

--- homecooking/cookingapp/models.py
+++ homecooking/cookingapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import UserManager, User
+from django.contrib.auth import authenticate
+import datetime
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class RecipeTry(models.Model):
+    date = models.DateField(default=timezone.now)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.date
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
