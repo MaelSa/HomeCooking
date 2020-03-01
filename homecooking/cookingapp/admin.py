e683a111ce19149a577c9969b686f49984d812cd
E19B287H <E19B287H>
2020-03-01 18:22:26 +0100

    admins

--- homecooking/cookingapp/admin.py
+++ homecooking/cookingapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Unit, Recipe, RecipeTry, RecipeStep, Ingredient, IngredientInRecipe, Comment
 
+
+admin.site.register(Recipe)
+admin.site.register(RecipeTry)
+admin.site.register(Unit)
+admin.site.register(IngredientInRecipe)
+admin.site.register(Ingredient)
 # Register your models here.
